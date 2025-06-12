48cbae28a2c6c499ac29b4d0374bd651a7129c8d
Aleksandr Mitin <mitinsoft@gmail.com>
2025-06-12 17:18:36 +0300

    refresh

---
+++ extjs/js/ext.js
@@ -0,0 +1,12 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-V8XCT0PHRY';
+  document.head.appendChild(script);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', 'G-V8XCT0PHRY');
+})();
